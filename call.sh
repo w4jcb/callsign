2dce15357edeea32ab418340adbae65ab84c7c11
w4jcb <carlton@w4jcb.com>
2020-05-14 09:59:16 -0400

    updating

--- call.sh
+++ call.sh
@@ -26,7 +26,6 @@ Callsign Lookup is a tool that will look up callsigns in the FCC *DAT* file
 EN.dat 
 
 By Carlton Bryan www.w4jcb.com (GNU/General Public License version 2.0)
-
 _EOF_
 }
 
@@ -41,10 +40,14 @@ read -p "Press Enter to continue"
 
 call_sign()
 {
+
+# clear
 # shows Callsign info
 
+callsign=$1
+
 #read user input
-read -p "Please enter callsign:" callsign
+# read -p "Please enter callsign:" callsign
 
 if [[ -z "$callsign" ]]; then
 	echo "No input entered"
@@ -55,7 +58,7 @@ CALL=$(echo $callsign | tr [:lower:] [:upper:])
 
 #search for callsign in EN.dat file and print details on the screen
 LOOKCALL=$(cat EN.dat | awk "/$CALL/ "' { print $0 }')
-# awk '/W4JCB/ { print }' EN.txt # This prints record 
+# awk '/B/ { print }' EN.txt # This prints record 
 
 NAME=$(echo $LOOKCALL | awk -F "|" '{print $9, $10, $11}')
 ADDRESS=$(echo $LOOKCALL | awk -F "|" '{print $17, $18, $19}')
@@ -75,14 +78,9 @@ selection="1"
 until [ $selection = " " ]; do
 
 cat << _EOF_
-Press the number of your choice:
 
+Enter Callsign or press q to quit:
 
-1 – Callsign lookup.
-2 – Help.
-3 - Exit Callsign lookup.
-
-Each number corresponds to a script function or a set of simple commands.
 _EOF_
 
 
@@ -90,7 +88,8 @@ read selection
 case $selection in
 1) call_sign;pas;clear;;
 2) help;pas;clear;;
-3) leave;pas;clear;;
-*) echo "Not a valid choice: Please try again.";pas;clear;;
+q) leave;pas;clear;;
+# *) echo "Not a valid choice: Please try again.";pas;clear;;
+*) clear;call_sign $selection;;
 esac
 done
